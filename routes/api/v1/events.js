77e4efa2020ed53e5ac4a52e4254b5fe2cfb0dde
Ellasimms1 <500207748@fvtc.edu>
2026-04-30 11:59:55 -0500

    set up the mongodb connection

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -1,42 +1,46 @@
 
-const router = require ('express').Router()
+const router = require("express").Router()
+const { getCollection, ObjectId } = require("../../../dbconnect")
 
 
-const events =[
-    {id:1, name: "Food Truck Festival", date: "2026-09-15", location: "Downtown", time: "12:00 PM"},
-    {id:2, name: "Night Market", date: "2026-09-22", location: "City Park", time: "5:00 PM"},
-    {id:3, name: "BBQ Cook-off", date: "2026-09-29", location: "Riverside", time: "1:00 PM"},
-    {id:4, name: "Street Food Fair", date: "2026-10-05", location: "Main Square", time: "11:00 AM"},
-    {id:5, name: "Food Truck Rally", date: "2026-10-12", location: "Beachfront", time: "3:00 PM"}
-]
+//const events =[
+   // {id:1, name: "Food Truck Festival", date: "2026-09-15", location: "Downtown", time: "12:00 PM"},
+   // {id:2, name: "Night Market", date: "2026-09-22", location: "City Park", time: "5:00 PM"},
+   // {id:3, name: "BBQ Cook-off", date: "2026-09-29", location: "Riverside", time: "1:00 PM"},
+   // {id:4, name: "Street Food Fair", date: "2026-10-05", location: "Main Square", time: "11:00 AM"},
+    //{id:5, name: "Food Truck Rally", date: "2026-10-12", location: "Beachfront", time: "3:00 PM"}
+//]
 
-// returns JSON object of events information/array when URL is GET /api/v1/events/
-router.get('/', (req, res) => {
-    res.json(events)
+
+
+router.get("/", async (req, res) => {
+    const collection = await getCollection("FoodtruckAPI", "EventItems")
+    const found = await collection.find({}).toArray()
+
+    res.send(found)
 })
 
-// returns JSON object of event with specified id when URL is GET /api/v1/events/:id
-router.get('/:id', (req, res) => {
-    const {id} =req.params
-    const found =events.find(item => item.id.toString() === id)
-    if (found) {
-        res.json(found)
-    } else {
-        res.status(404).json({ message: "Event not found" })
-    }
+router.get("/:id", async (req, res) => {
+    const { id } = req.params
+
+    const collection = await getCollection("FoodtruckAPI", "EventItems")
+
+    const found = await collection.findOne({
+        _id: new ObjectId(id)
+    })
+
+    if(found) res.send(found)
+    else res.send({ error: { message: "Event not found" } })
 })
 
-// add new event to DB when URL is POST /api/v1/events/
-router.post ('/', (req, res) => {
-    const {name, date, location, time} = req.body
-    const newEvent = {
-        id: events.length + 1,
-        name,
-        date,
-        location,
-        time
-    }
-    events.push(newEvent)
-    res.status(201).json(newEvent)
+router.post("/", async (req, res) => {
+    const collection = await getCollection("FoodtruckAPI", "EventItems")
+
+    await collection.insertOne(req.body)
+
+    res.send({ message: "Event added" })
 })
+
 module.exports = router
+
+ 
--- routes/api/v1/menu.js
+++ routes/api/v1/menu.js
@@ -1,45 +1,44 @@
-const path = require ('path') 
-const router = require ('express').Router()
 
-const menu = [
-    {id:1, name: "pizza", description: "Cheesy goodness ", price: 9.99, image: "/images/pizza.jpg"},
-    {id:2, name: "burger", description: "Juicy beef patty", price: 11.99, image: "/images/burger.jpg"},
-    {id:3, name: "taco", description: "Spicy chicken wrap", price: 8.99, image: "/images/taco.jpg"},
-    {id:4, name: "hotdog", description: "Grilled sausage in a bun", price: 4.99, image: "/images/hotdog.jpg"},
-    {id:5, name: "fries", description: "Crispy golden loaded fries", price: 3.99, image: "/images/fries.jpg"}
-]
+const router = require("express").Router()
+const { getCollection, ObjectId } = require("../../../dbconnect")
 
-// returns JSON object of menu array/information when URL is GET api/v1/menu/
-router.get('/', (req, res) => {
-    res.json(menu)
-    
+//const menu = [
+   // {id:1, name: "pizza", description: "Cheesy goodness ", price: 9.99, image: "/images/pizza.jpg"},
+    //{id:2, name: "burger", description: "Juicy beef patty", price: 11.99, image: "/images/burger.jpg"},
+   // {id:3, name: "taco", description: "Spicy chicken wrap", price: 8.99, image: "/images/taco.jpg"},
+   // {id:4, name: "hotdog", description: "Grilled sausage in a bun", price: 4.99, image: "/images/hotdog.jpg"},
+    //{id:5, name: "fries", description: "Crispy golden loaded fries", price: 3.99, image: "/images/fries.jpg"}
+//]
+
+
+
+router.get("/", async (req, res) => {
+    const collection = await getCollection("FoodtruckAPI", "MenuItems")
+    const found = await collection.find({}).toArray()
+
+    res.send(found)
 })
 
-// returns JSON object that contains menu item with specified id when URL is GET api/v1/menu/:id
-router.get('/:id', (req, res) => {
-    const {id} =req.params
-    const found =menu.find(item => item.id.toString() === id)
-    if (found) {
-        res.json(found)
-    } else {
-        res.status(404).json({ message: "Menu item not found" })
-    }
-    
+router.get("/:id", async (req, res) => {
+    const { id } = req.params
+
+    const collection = await getCollection("FoodtruckAPI", "MenuItems")
+
+    const found = await collection.findOne({
+        _id: new ObjectId(id)
+    })
+
+    if(found) res.send(found)
+    else res.send({ error: { message: "Menu item not found" } })
 })
 
-// adds new menu item to DB when URL is POST /api/v1/menu/
-router.post ('/', (req, res) => {
-    const {name, description, price, image} = req.body
-    const newItem = {
-        id: menu.length + 1,
-        name,
-        description,
-        price,
-        image
-       
-    }
-    menu.push(newItem)
-    res.status(201).json(newItem)
+router.post("/", async (req, res) => {
+    const collection = await getCollection("FoodtruckAPI", "MenuItems")
 
+    await collection.insertOne(req.body)
+
+    res.send({ message: "Menu item added" })
 })
+
 module.exports = router
+
